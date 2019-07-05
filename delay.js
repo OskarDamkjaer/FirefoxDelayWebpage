0d0ca5ac4191a009d81a22fa2a92f7ce8a83c6f2
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2019-07-05 09:02:05 +0200

    add manifest

--- delay.js
+++ delay.js
@@ -18,9 +18,22 @@ function handleVisibilityChange() {
 
 const blocking_div = document.createElement("div");
 blocking_div.id = "__dly_id__"
-blocking_div.style.cssText = 'background-color:#3CAEA3;position:fixed;top:0;left:0;width:100vw;height:10000px;text-align:center;font-size:7em;padding-top:5rem;z-index:999999999;color:black;'
+blocking_div.style.cssText = `background-color:#3CAEA3;
+font-size:7vw;
+padding-top:7vh;
+position:fixed;
+top:0;
+left:0;
+width:100vw;
+height:10000px;
+text-align:center;
+z-index:999999999;
+line-height:normal;
+font-weight: normal; 
+color:black;`
 blocking_div.appendChild(document.createTextNode("Wait five seconds for the page to load"));
 document.body.appendChild(blocking_div);
+
 document.addEventListener("visibilitychange", handleVisibilityChange, false);
 removeBlockingDiv()
 let timeout;
