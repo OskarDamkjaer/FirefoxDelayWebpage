9012b9ca33744e6ffeef0cdfdf8f8a4d8707dfad
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2019-12-08 13:25:44 +0100

    move delay back to root

---
+++ delay.js
@@ -0,0 +1,39 @@
+function removeBlockingDiv() {
+    timeout = setTimeout(() => {
+        if (document.visibilityState === "visible") {
+            document.getElementById("__dly_id__").remove()
+            document.removeEventListener("visibilitychange", handleVisibilityChange, false)
+        }
+    }, 7000)
+}
+
+// Handle when tab gains focus
+function handleVisibilityChange() {
+    if (document.visibilityState === "visible") {
+        removeBlockingDiv()
+    } else {
+        clearTimeout(timeout)
+    }
+}
+
+const blocking_div = document.createElement("div");
+blocking_div.id = "__dly_id__"
+blocking_div.style.cssText = `background-color:#72a3dd;
+font-size:7vw;
+padding-top:7vh;
+position:fixed;
+top:0;
+left:0;
+width:100vw;
+height:10000px;
+text-align:center;
+z-index:999999999;
+line-height:normal;
+font-weight: normal; 
+color:#FFF;`
+blocking_div.appendChild(document.createTextNode("Wait seven seconds for the page to load"));
+document.body.appendChild(blocking_div);
+
+document.addEventListener("visibilitychange", handleVisibilityChange, false);
+removeBlockingDiv()
+let timeout;
